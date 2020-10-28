38fe6014efabaa547b91d22d8f724f308526a556
Teja Naidu Chintha <50400006+ctejanaidu@users.noreply.github.com>
2020-10-28 15:00:43 +0530

    Update process.php

--- process.php
+++ process.php
@@ -4,9 +4,18 @@
 	$password = $_POST['password'];
 
 	
-  
+	$username = stripcslashes($username);
+	$password = stripcslashes($password);
+	
+
+
+	$con=mysqli_connect("localhost","root", "","login");
+
+	$result = mysqli_query($con,"select * from user where username = '$username' and password = '$password'") 
+					or die("Failed to query database".mysql_error());
+	$row = mysqli_fetch_array($result);
 	if ($row['username'] == $username && $row['password'] == $password) {
-		echo "Login Success!! Welcome".row['username'];
+		echo "Login Success!! Welcome";
 	} else {
 		echo "Failed to Login";
 	}
